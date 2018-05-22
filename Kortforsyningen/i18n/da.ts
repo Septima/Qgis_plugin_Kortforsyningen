03835b282886b81b433e320ee14a43a603d6335a
mie winstrup <mie@septima.dk>
2018-05-22 09:54:09 +0200

    Oversættelse En --> Dk (tokens etc.)

--- Kortforsyningen/i18n/da.ts
+++ Kortforsyningen/i18n/da.ts
@@ -13,96 +13,78 @@
     <message>
         <location filename="aboutKortforsyningenAlternative.ui" line="22"/>
         <source>About the plugin</source>
-        <translation>Om dette plugin</translation>
-    </message>
-</context>
-<context>
-    <name>KfConfig</name>
-    <message>
-        <location filename="kf_config.py" line="158"/>
-        <source>No contact to Kortforsyningen</source>
-        <translation>Ingen kontakt til Kortforsyningen</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>Kortforsyningen</name>
     <message>
-        <location filename="kortforsyningen.py" line="194"/>
+        <location filename="kortforsyningen_REMOTE_3636.py" line="172"/>
         <source>Kortforsyningen</source>
         <translation>Kortforsyningen</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="244"/>
+        <location filename="kortforsyningen.py" line="128"/>
+        <source>Token not set or wrong. Click menu Kortforsyningen-&gt;Settings</source>
+        <translation>Token er ikke angivet. Klik på menu Kortforsyningen-&gt;Indstillinger</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen_REMOTE_3636.py" line="222"/>
         <source>Settings</source>
         <translation>Indstillinger</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="254"/>
+        <location filename="kortforsyningen_REMOTE_3636.py" line="232"/>
         <source>About the plugin</source>
         <translation>Om dette plugin</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="148"/>
-        <source>No contact to Kortforsyning</source>
-        <translation>Ingen kontakt til Kortforsyningen</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="170"/>
-        <source>Username/Password not set or wrong. Click menu Kortforsyningen-&gt;Settings</source>
-        <translation>Brugernavn/kodeord er ikke indtastet eller er forkert. Klik på Kortforsyningsmenu --&gt; Indstillinger</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="336"/>
+        <location filename="kortforsyningen_REMOTE_3636.py" line="314"/>
         <source>&lt;p&gt;QGIS is having trouble showing the content of this dialog. Would you like to open it in an external browser window?&lt;/p&gt;</source>
         <translation>&lt;p&gt;QGIS har problemer med at vise indholdet af denne dialogboks. Ønsker du at vise det i et eksternt browservindue?&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="kortforsyningen_REMOTE_3636.py" line="152"/>
+        <source>Username/Password not set or wrong. Click menu Kortforsyningen-&gt;Settings</source>
+        <translation>Token ikke korrekt angivet. Klik menu Kortforsyningen-&gt;Indstillinger</translation>
     </message>
 </context>
 <context>
     <name>s</name>
     <message>
-        <location filename="kf_settings.ui" line="50"/>
-        <source>Username:</source>
-        <translation>Brugernavn:</translation>
-    </message>
-    <message>
-        <location filename="kf_settings.ui" line="60"/>
-        <source>Password:</source>
-        <translation>Kodeord:</translation>
-    </message>
-    <message>
-        <location filename="kf_settings.ui" line="14"/>
+        <location filename="kf_settings.ui" line="23"/>
         <source>Kortforsyningen - Settings</source>
         <translation>Kortforsyningen - Indstillinger</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="44"/>
-        <source>User information at Kortforsyningen</source>
-        <translation>Brugerinformation til Kortforsyningen</translation>
-    </message>
-    <message>
-        <location filename="kf_settings.ui" line="87"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;      Not registered as a user at Kortforsyningen? &lt;a href=&quot;http://download.kortforsyningen.dk//content/opret-mig-som-bruger&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create user&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;      Ikke registreret som bruger på Kortforsyningen? &lt;a href=&quot;http://download.kortforsyningen.dk//content/opret-mig-som-bruger&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Opret bruger&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <location filename="kf_settings.ui" line="93"/>
+        <location filename="kf_settings.ui" line="59"/>
         <source>Extend Kortforsyningsplugin with qlr-file</source>
         <translation>Udvid Kortforsyningsplugin med qlr-fil</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="143"/>
-        <source>Select qlr-file</source>
-        <translation>Vælg qlr-fil</translation>
-    </message>
-    <message>
-        <location filename="kf_settings.ui" line="134"/>
+        <location filename="kf_settings.ui" line="126"/>
         <source>Only display backgroundmap from Kortforsyningen</source>
         <translation>Vis kun baggrundskort fra Kortforsyningen</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="80"/>
-        <source>Save username and password</source>
-        <translation>Gem brugernavn og kodeord</translation>
+        <location filename="kf_settings.ui" line="136"/>
+        <source>Select qlr-file</source>
+        <translation>Vælg qlr-fil</translation>
+    </message>
+    <message>
+        <location filename="kf_settings.ui" line="166"/>
+        <source>User at Kortforsyningen</source>
+        <translation>Kortforsyningsbruger</translation>
+    </message>
+    <message>
+        <location filename="kf_settings.ui" line="187"/>
+        <source>Enter token to Kortforsyning</source>
+        <translation>Angiv token til Kortforsyningen</translation>
+    </message>
+    <message>
+        <location filename="kf_settings.ui" line="194"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Find your token to Kortforsyningen (link).&lt;/p&gt;&lt;p&gt;Not registered as a user at Kortforsyningen? &lt;a href=&quot;http://download.kortforsyningen.dk//content/opret-mig-som-bruger&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create user &lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Find dit token til Kortforsyningen (link).&lt;/p&gt;&lt;p&gt;Ikke registreret som Kortforsyningsbruger? &lt;a href=&quot;http://download.kortforsyningen.dk//content/opret-mig-som-bruger&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Opret bruger &lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
